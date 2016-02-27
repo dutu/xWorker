3272aba2d14f0ac137c7cc055248a0282e74bea2
dutu <adrian.clinciu@outlook.com>
2016-02-27 16:51:54 +0200

    add socket.io

--- server.js
+++ server.js
@@ -1,44 +1,97 @@
-var WebSocketServer = require("ws").Server;
-var http = require("http");
-var express = require("express");
+
 var winston = require("winston");
-var async = require("async");
-var _ = require("lodash");
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var morgan = require('morgan');
+var path = require('path');
+var http = require('http');
 
 var Workers = require('./server/workers/workers');
 var srv = require ("./server/core/srv");
 var config = require("./server/core/config");
 var logger = srv.logger;
 
-var app = express();
-var port = process.env.PORT || 5000;
+var routes = require('./routes');
+var api = require('./routes/api');
 
-
-app.use(express.static(__dirname + "/public"));
+var app = module.exports = express();
 
 var server = http.createServer(app);
-server.listen(port);
+var io = require('socket.io')(server);
+srv.io = io;
 
-logger.setLevels(winston.config.syslog.levels);
-logger.info("http server listening on %d", port);
+logger.info("Socket.io server created");
 
-var wss = new WebSocketServer({server: server});
-logger.info("websocket server created");
+/**
+ * Configuration
+ */
 
-wss.on("connection", function(ws) {
-	srv.ws = ws;
+// all environments
 
-	var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000);
 
-  logger.info("websocket connection open");
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(morgan('dev'));
 
-  ws.on("close", function() {
-    logger.info("websocket connection close");
-    clearInterval(id)
-  })
+// app.use(bodyParser()); // body-parser deprecated bodyParser: use individual json/urlencoded middlewares
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('json spaces', 4);
+
+
+// development only
+if (process.env.NODE_ENV === 'development') {
+	app.use(express.errorHandler());
+}
+
+// production only
+if (env === 'production') {
+	// TODO
+}
+
+/**
+ * Routes
+ */
+
+// serve index and view partials
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// JSON API
+app.get('/api/name', api.name);
+/*
+ app.get('/api/post/:id', api.post);
+ app.post('/api/post', api.addPost);
+ app.put('/api/post/:id', api.editPost);
+ app.delete('/api/post/:id', api.deletePost);
+ */
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+// Socket.io Communication
+io.sockets.on('connection',  function (socket) {
+	socket.emit('send:name', {
+		name: 'Bob'
+	});
 });
+
+/**
+ * Start Server
+ */
+
+server.listen(app.get('port'), function () {
+	logger.info('Express server listening on port ' + app.get('port'));
+});
+
 
 /*
 var i, signals = ["SIGTERM"];
@@ -48,6 +101,8 @@ for (i in signals) {
 	});
 }
 */
+
+
 
 srv.config = _.cloneDeep(config);
 
